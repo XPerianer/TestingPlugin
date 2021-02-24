ab117a60121ca252286522d60448b673bc13a850
XPerianer <dominik.meier@student.hpi.de>
2021-02-24 13:58:12 +0100

    lint

--- src/extension.ts
+++ src/extension.ts
@@ -22,23 +22,16 @@
  * THE SOFTWARE.
  */
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import { pathToFileURL } from 'url';
 import * as vscode from 'vscode';
-import { relative } from 'path';
-import { Test } from 'mocha';
 
 import TestingPluginPanel from './testingPluginPanel';
 
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('testingPlugin.start', () => {
 			TestingPluginPanel.createOrShow(context.extensionUri);
-		}))
+		}));
 	if (vscode.window.registerWebviewPanelSerializer) {
 		// Make sure we register a serializer in activation event
 		vscode.window.registerWebviewPanelSerializer('testingPlugin', {
@@ -52,7 +45,7 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('testingPlugin.save', () => {
 			TestingPluginPanel.save();
 		})
-	)
+	);
 
 	// Register to change events
 	vscode.window.onDidChangeVisibleTextEditors(TestingPluginPanel.onDidChangeVisibleTextEditors.bind(TestingPluginPanel));
